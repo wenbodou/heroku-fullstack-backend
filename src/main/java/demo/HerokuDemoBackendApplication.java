b2c44c68bef16fb3c048b83264bcecea87febe1c
WenboDou <wenbodou2012@gmail.com>
2021-06-19 22:52:37 -0400

    'crossorigin'

--- src/main/java/demo/HerokuDemoBackendApplication.java
+++ src/main/java/demo/HerokuDemoBackendApplication.java
@@ -2,8 +2,10 @@ package demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EnableWebMvc
 public class HerokuDemoBackendApplication {
 
 	public static void main(String[] args) {
--- src/main/java/demo/controller/HelloWorldController.java
+++ src/main/java/demo/controller/HelloWorldController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import demo.model.TestTable;
 import demo.service.TestService;
 
+@CrossOrigin("*")
 @Controller
 public class HelloWorldController {
 	@Autowired
